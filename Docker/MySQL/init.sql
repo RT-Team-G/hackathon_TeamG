35d4786199e9a07b7044bdfb65af8c8a79d961c7
Takumi <itriandaiskida@gmail.com>
2026-04-26 20:06:56 +0900

    全テーブル作成とテスト作成中

--- Docker/MySQL/init.sql
+++ Docker/MySQL/init.sql
@@ -90,6 +90,60 @@ CREATE TABLE
         KEY idx_comments_user_id (user_id),
         KEY idx_comments_post_id (post_id),
         --外部キー
-        CONSTRAINT fk_comments_user_id FOREIGN KEY (user_id) REFERENCES Users (id),
-        CONSTRAINT fk_comments_post_id FOREIGN KEY (post_id) REFERENCES Posts (id)
+        CONSTRAINT fk_comments_user FOREIGN KEY (user_id) REFERENCES Users (id),
+        CONSTRAINT fk_comments_post FOREIGN KEY (post_id) REFERENCES Posts (id)
     ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Post_Reactionテーブルの作成
+CREATE TABLE 
+    PostReaction (
+        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        user_id BIGINT UNSIGNED NOT NULL,
+        post_id BIGINT UNSIGNED NOT NULL,
+        reaction_id BIGINT UNSIGNED NOT NULL,
+        created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        --主キー
+        PRIMARY KEY (id),
+        --インデクス索引
+        KEY idx_post_reaction_user_id (user_id),
+        KEY idx_post_reaction_post_id (post_id),
+        KEY idx_post_reaction_reaction_id (reaction_id),
+        --外部キー
+        CONSTRAINT fk_post_reaction_user FOREIGN KEY (user_id) REFERENCES Users (id),
+        CONSTRAINT fk_post_reaction_post FOREIGN KEY (post_id) REFERENCES Posts (id),
+        CONSTRAINT fk_post_reaction_reaction FOREIGN KEY (reaction_id) REFERENCES Reactions (id)
+    ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+-- Post_Trainingテーブル作成
+CREATE TABLE 
+   Post_Training (
+        id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+        user_id BIGINT UNSIGNED NOT NULL,
+        post_id BIGINT UNSIGNED NOT NULL,
+        training_id BIGINT UNSIGNED NOT NULL,
+        reps INT,
+        time INT,
+        set INT,
+        created_at DATETIME(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6),
+        --主キー
+        PRIMARY KEY (id),
+        --インデックス索引
+        KEY idx_post_training_user_id (user_id),
+        KEY idx_post_training_post_id (post_id),
+        KEY idx_post_training_training_id (training_id),
+        --外部キー
+        CONSTRAINT fk_post_training_user FOREIGN KEY (user_id) REFERENCES Users (id),
+        CONSTRAINT fk_post_training_post FOREIGN KEY (post_id) REFERENCES Posts (id),
+        CONSTRAINT fk_post_training_training FOREIGN KEY (training_id) REFERENCES Training (id)
+   ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
+
+--テスト用 Users
+INSERT INTO Users (name, email, password) VALUES 
+('佐藤一郎', 'sato@example', '12345'),
+('鈴木次郎', 'suzuki@example', '98765')
+
+-- Posts
+INSERT INTO Posts (user_id, content) VALUES 
+(1, 'こんにちは!初めての投稿です'),
+(1, '今日は腕立てふせ10回やりました！'),
+(2, '今日はスクワット1000かいやりました。')
