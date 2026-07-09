6c48dc74eb4971adc6f0eaca4f18f1675ad18404
EC2 Default User <ec2-user@ip-10-0-0-108.ap-northeast-1.compute.internal>
2026-07-09 15:09:26 +0000

    DBコネクション切れた時の再接続設定追加

--- SNSApp/models/comment.py
+++ SNSApp/models/comment.py
@@ -11,6 +11,7 @@ class Comments:
     #コメント作成
     def create(cls, user_id, post_id, content):
         conn = db_pool.get_conn()
+        conn.ping(reconnect=True)
         try:
             #カーソル作成
             with conn.cursor() as cur:
--- SNSApp/models/reactions.py
+++ SNSApp/models/reactions.py
@@ -12,6 +12,7 @@ class Reactions:
     def get_reaction(cls, post_id):
         # プールから接続
         conn = db_pool.get_conn()
+        conn.ping(reconnect=True)
         try:
             # カーソル作成
             with conn.cursor() as cur:
